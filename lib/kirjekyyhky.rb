5abd8e2251a8d3af9a2aeb2aa97c448352364910
Karl Karlberg <karl.karlberg@cs.helsinki.fi>
2013-04-11 12:30:40 +0300

    Improved Kirjekyyhky::Interface.send_form_structure slightly

--- lib/kirjekyyhky.rb
+++ lib/kirjekyyhky.rb
@@ -4,15 +4,10 @@ require 'net/https'
 module Kirjekyyhky 
   class Interface
     BOUNDARY = "AaB03x"
-    def self.send_form_structure 
+    def self.send_form_structure file 
       credentials = YAML.load(File.open(Rails.root.join("config/kirjekyyhky.yml")))
 
       uri = URI("#{credentials["url"]}/post/structure/")
-      p "uri: #{credentials["url"]}/post/structure"
-      p "username: #{credentials["username"]}"
-      p "password: #{credentials["password"]}"
-
-      file = "lib/vesilintu-testi.xml"
 
       post_body = []
       post_body << "--#{BOUNDARY}\r\n"
@@ -33,19 +28,11 @@ module Kirjekyyhky
 
       res = http.request(req)
 
-#      req.add_field "Content-Type", "application/xml; charset=\"UTF-8\""
-#      xml = IO.read(Rails.root.join("lib/vesilintu-testi.xml"))
-#      req.body = xml
-
-#      res = http.start do |http|
-#        http.request(req)
-#      end
       if res.code == "200"
         return res.body
       else
         throw "Response failed, code: #{res.code}, body: #{res.body}"
       end
     end
-
   end
 end
